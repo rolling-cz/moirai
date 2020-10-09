085e4a79165e59731b3de2ef1d3deb431d9670e8
Tomas Fechtner <fechtnertomas@gmail.com>
2020-10-09 18:56:42 +0200

    show app version

--- src/main/java/cz/rolling/moirai/controller/IndexController.java
+++ src/main/java/cz/rolling/moirai/controller/IndexController.java
@@ -1,12 +1,25 @@
 package cz.rolling.moirai.controller;
 
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping({"/"})
 public class IndexController {
+
+    private final BuildProperties buildProperties;
+
+    public IndexController(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
+    @ModelAttribute("version")
+    public String version() {
+        return buildProperties.getVersion();
+    }
 
     @GetMapping({"/"})
     public String index() {
