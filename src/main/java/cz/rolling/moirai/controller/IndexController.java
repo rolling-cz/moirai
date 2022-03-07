621828642cf34b4e6697a754c0335758e7b08092
fechtner <fechtnertomas@gmail.com>
2022-03-07 21:05:48 +0100

    version info

--- src/main/java/cz/rolling/moirai/controller/IndexController.java
+++ src/main/java/cz/rolling/moirai/controller/IndexController.java
@@ -1,24 +1,35 @@
 package cz.rolling.moirai.controller;
 
-import org.springframework.boot.info.BuildProperties;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequestMapping({"/"})
 public class IndexController {
 
-    private final BuildProperties buildProperties;
+    @Value("${git.commit.id}")
+    private String commitId;
 
-    public IndexController(BuildProperties buildProperties) {
-        this.buildProperties = buildProperties;
-    }
+    @Value("${git.commit.id.abbrev}")
+    private String commitAbbrev;
 
-    @ModelAttribute("version")
-    public String version() {
-        return buildProperties.getVersion();
+    @Value("${git.build.time}")
+    private String buildTime;
+
+    @ModelAttribute("versionInfo")
+    public Map<String, String> versionInfo() {
+        Map<String, String> versionInfo = new HashMap<>();
+        versionInfo.put("commitId", commitId);
+        versionInfo.put("commitAbbrev", commitAbbrev);
+        versionInfo.put("buildTime", StringUtils.split(buildTime, "T")[0]);
+        return versionInfo;
     }
 
     @GetMapping({"/"})
